61b162666520a0c78e392514c62c6ad2852d43e5
Sibi Chandra Sekar <87488626+sibi1702@users.noreply.github.com>
2025-02-22 12:47:47 -0600

    List

--- sample.py
+++ sample.py
@@ -23,3 +23,7 @@ print(thislist)
 list1 = ["apple", "banana", "cherry"]
 list2 = [1, 5, 7, 9, 3]
 list3 = [True, False, False]
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
