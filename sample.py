5d893917cef61d5853b4653364ef74a66d206830
Sibi Chandra Sekar <87488626+sibi1702@users.noreply.github.com>
2025-02-22 12:32:07 -0600

    initial commit

---
+++ sample.py
@@ -0,0 +1,15 @@
+def test_function():
+    x = "Test"
+    print("Hello "+x)
+
+test_function()
+
+a = {"name" : "John", "age" : 36}
+print(type(a))
+
+b = ["apple", "banana", "cherry"]	
+print(type(b))
+
+c = ("apple", "banana", "cherry")	
+print(type(c))
+
